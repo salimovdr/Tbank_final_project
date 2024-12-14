5f2e47c186e23db8b5603c0218a4645f75516e7e
FMariaD <71096257+FMariaD@users.noreply.github.com>
2024-12-14 13:45:48 +0300

    Update PhotoStorageManager.py

--- src/PhotoStorageManager.py
+++ src/PhotoStorageManager.py
@@ -9,13 +9,13 @@ class PhotoStorageManager:
 
     @staticmethod
     def manage_photos(user_dir, max_photos):
-        files = sorted([f for f in os.listdir(user_dir) if f.isdigit()], key=int, reverse=True)
+        files = os.listdir(user_dir)
 
-        for i in range(len(files), 0, -1):
-            old_name = os.path.join(user_dir, str(i))
-            new_name = os.path.join(user_dir, str(i + 1))
-            if os.path.exists(old_name):
-                os.rename(old_name, new_name)
-
-        if len(files) > int(max_photos):
+        if len(files) >= int(max_photos):
             os.remove(os.path.join(user_dir, files[0]))
+            for i in range(1, len(files)):
+                old_name = os.path.join(user_dir, str(i))
+                new_name = os.path.join(user_dir, str(i - 1))
+                if os.path.exists(old_name):
+                    os.rename(old_name, new_name)
+                    
